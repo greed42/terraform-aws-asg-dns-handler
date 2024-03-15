394ea0b24aff505936bd9901e400801812c76848
Graham Reed <greed@hypervolt.co.uk>
2024-03-15 16:16:54 +0000

    Explicit provider requirements

--- versions.tf
+++ versions.tf
@@ -1,4 +1,15 @@
-
 terraform {
   required_version = ">= 0.12"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.4.0"
+    }
+  }
 }
